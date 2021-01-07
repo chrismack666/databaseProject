524cd2cbea6a638b8621abf987cc3fe2dd6f67d3
chrismack666 <59908458+chrismack666@users.noreply.github.com>
2021-01-07 15:09:07 +0700

    added validation

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,17 +1,9 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.validation.Valid;
-import java.util.Date;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @Controller
@@ -20,33 +12,22 @@ public class MainController {
     private ReminderRepository reminderRepository;
 
     @GetMapping("/")
-    public String mainList(Map<String, Object> model) {
+    public String mainList(Model model) {
         Iterable<Reminder> reminders = reminderRepository.findAll();
-        model.put("reminders", reminders);
+        model.addAttribute("reminders", reminders);
         return "listReminders";
     }
 
     @GetMapping("/add")
-    public String mainAdd(Map<String, Object> model) {
-        Iterable<Reminder> reminders = reminderRepository.findAll();
-        model.put("reminders", reminders);
+    public String mainAdd(Model model) {
+        model.addAttribute("reminder", new Reminder());
         return "addReminder";
     }
 
     @PostMapping("/add")
-    public String add(@Valid Reminder reminder, BindingResult bindingResult, @RequestParam String text, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd")Date date, Map<String, Object> model) {
-        if (bindingResult.hasErrors()) {
-            return "addReminder";
-        } else {
-            reminder = new Reminder(text, date);
-
-            reminderRepository.save(reminder);
-
-            Iterable<Reminder> reminders = reminderRepository.findAll();
-
-            model.put("reminders", reminders);
-            return "listReminders";
-        }
+    public String add(Reminder reminder) {
+        reminderRepository.save(reminder);
+        return "redirect:/";
     }
 
     @PostMapping("/")
--- src/main/java/com/example/demo/Reminder.java
+++ src/main/java/com/example/demo/Reminder.java
@@ -1,14 +1,12 @@
 package com.example.demo;
 
-import com.sun.istack.NotNull;
+;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.FutureOrPresent;
-import javax.validation.constraints.NotEmpty;
 import java.util.Date;
 
 @Entity // This tells Hibernate to make a table out of this class
@@ -17,22 +15,10 @@ public class Reminder {
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
 
-    @NotEmpty(message = "Reminder's text should not be empty")
-    @FutureOrPresent(message = "Enter the future date")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date date;
 
-    @NotEmpty(message = "Reminder's text should not be empty")
     private String text;
-
-    public Reminder(String text, Date date) {
-        this.text = text;
-        this.date = date;
-    }
-
-    public Reminder() {
-
-    }
 
     public Integer getId() {
         return id;
